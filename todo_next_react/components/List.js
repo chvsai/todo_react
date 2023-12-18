16ca7d89adde42d81970e1cac33f4213eb676b2a
chvsai <chadaramlakshmi@gmail.com>
2023-12-19 00:31:17 +0900

    next todo app

---
+++ todo_next_react/components/List.js
@@ -0,0 +1,5 @@
+import List_item from "./List_item";
+
+export default function(props){
+    return (<List_item />);
+}
---
+++ todo_next_react/components/List_item.js
@@ -0,0 +1,11 @@
+export default function(props){
+    return (<div>
+        <span>
+            <text>
+                {props.detail}
+            </text>
+        </span>
+        <div>X</div>
+        <button>Delete</button>
+    </div>);
+}
